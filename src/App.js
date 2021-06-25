6a7352e46b6580d5d1423fc3003bff36f7d18686
Jonhks <jonhgx@gmail.com>
2021-06-24 20:17:21 -0500

    Create/creando repo, aun no lo terminamos en la clase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import Formulario from './components/Formulario';
+import Error from './components/Error';
+import ListadoImagenes from './components/ListadoImagenes';
 
-function App() {
+const App = () => {
+
+  const [busqueda, setBusqueda] = useState('');
+  const [imagenes, setImagenes] = useState([]);
+  const [peticionVacia, setPeticionVacia] = useState(false);
+  const [paginaActual, setPaginaActual] = useState(4);
+  const [totalPaginas, setTotalPaginas] = useState(1);
+
+  useEffect(() => {
+
+    const consultarApi = async () => {
+      if(busqueda === '') return;
+  
+      const imagenesPorPagina = 30;
+      const key = '17997109-6aacfb96d3c78d72b639c185f';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina} `;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      if(resultado.total === 0 ){
+        setPeticionVacia(true)
+        return;
+      }
+      setPeticionVacia(false)
+      setImagenes(resultado.hits);
+
+      const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina)
+      setTotalPaginas(calcularTotalPaginas)
+    }
+
+    consultarApi();
+
+  },[busqueda])
+
+
+  // ir a la pagina anterior
+
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaActual - 1;
+    if(nuevaPaginaActual === 0) return;
+    setPaginaActual(nuevaPaginaActual)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">
+          Buscador de Imagenes
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Formulario
+          setBusqueda={setBusqueda}
+        />
+      </div>    
+      <div className="row justify-content-center">
+      {peticionVacia && <Error mensaje="No hay datos para mostrar" />}
+        <ListadoImagenes
+          imagenes={imagenes}
+        />
+        <button 
+          type ="button"
+          className="bbtn btn-info mr-1"
+          onClick={paginaAnterior}
         >
-          Learn React
-        </a>
-      </header>
+           &laquo; Anterior
+        </button>
+
+        <button 
+          type ="button"
+          className="bbtn btn-info mr-1"
+        >
+          Siguiente &raquo; 
+        </button>
+      </div>  
     </div>
   );
 }
